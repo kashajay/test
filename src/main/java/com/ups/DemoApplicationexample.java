3bdd0b42d57f212e24d7e8f0525d4181f8039128
kashajay <kashajay@gmail.com>
2017-03-10 12:16:12 -0500

    Update DemoApplicationexample.java

--- src/main/java/com/ups/DemoApplicationexample.java
+++ src/main/java/com/ups/DemoApplicationexample.java
@@ -2,7 +2,7 @@ package com.ups;
 
 import static spark.Spark.*;
 
-public class DemoApplication {
+public class DemoApplicationexample {
     public static void main(String[] args) {
         //get("/hello", (req, res) -> "Hello World");
         System.out.println("Hello World");

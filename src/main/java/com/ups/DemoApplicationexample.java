01f7a400319b3774cb8c42ceb0657ba79a86ca38
kashajay <kashajay@gmail.com>
2017-03-10 13:33:34 -0500

    Update DemoApplicationexample.java

--- src/main/java/com/ups/DemoApplicationexample.java
+++ src/main/java/com/ups/DemoApplicationexample.java
@@ -4,7 +4,14 @@ import static spark.Spark.*;
 
 public class DemoApplicationexample {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        //get("/hello", (req, res) -> "Hello World");
+       get("/", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!!";
+            }
+        });
+        
         System.out.println("Hello World");
     }
 }
